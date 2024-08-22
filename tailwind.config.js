584d88b5e8d495eadc0fffcfa17700a6da9278c7
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2024-08-21 21:16:42 -0300

    commit init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary-color': '#2f2f2f',
+        'secondary-color': '#0a0a23',
+        'brother-color': '#2a2a2a',
+        'text-color': '#ffffff',
+        'secondary-text-color': '#ccc',
+        'shadow-moon' : '#087ec4',
+        'shadow-orange': '#cc8338'
+      }
+    },
+  },
+  plugins: [],
+}
